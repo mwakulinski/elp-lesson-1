a1dbc0f33c41ce4631d9fcd6c1dd169dcc1e066a
mwakulinski <michal.wakulinski1@gmail.com>
2022-05-04 10:57:02 +0200

    correctly define class instance

--- src/NokiaTexter.test.ts
+++ src/NokiaTexter.test.ts
@@ -1,5 +1,9 @@
+class NokiaTexter {
+  constructor(private readonly text: string) {}
+}
+
 describe("NokiaTexter Test", () => {
-  it("should define class instance", () => {
+  it("should correctly define class instance", () => {
     expect(new NokiaTexter("Hi it should work")).toBeDefined();
   });
 });
