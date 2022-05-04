8a5f75c907bc9521ccc440a7a7931cc96452cd89
mwakulinski <michal.wakulinski1@gmail.com>
2022-05-04 12:01:55 +0200

    refactor: change squeeze() to user private methods

--- src/NokiaTexter.test.ts
+++ src/NokiaTexter.test.ts
@@ -3,21 +3,45 @@ class NokiaTexter {
   squeeze(): string {
     const trimmedText = this.text.trim();
 
-    if (!trimmedText.includes(" ")) return trimmedText;
+    if (this.notIncludesWhitespace(trimmedText)) return trimmedText;
 
     const squeezedText = trimmedText
       .split(" ")
-      .filter((word) => {
-        return word !== "";
-      })
-      .map((word, index) => {
-        return index % 2 === 0 ? word.toUpperCase() : word.toLowerCase();
-      })
+      .filter(this.isNotEmptyString())
+      .map(this.lowerCaseOddAndUpperCaseEvenWords())
       .join("");
 
-    if (squeezedText.length > 160) throw new Error("Sentance is to long");
+    if (this.isToLong(squeezedText)) throw new Error("Sentance is to long");
 
     return squeezedText;
+  }
+
+  private isToLong(squeezedText: string) {
+    return squeezedText.length > 160;
+  }
+
+  private notIncludesWhitespace(trimmedText: string) {
+    return !trimmedText.includes(" ");
+  }
+
+  private lowerCaseOddAndUpperCaseEvenWords(): (
+    value: string,
+    index: number,
+    array: string[]
+  ) => string {
+    return (word, index) => {
+      return index % 2 === 0 ? word.toUpperCase() : word.toLowerCase();
+    };
+  }
+
+  private isNotEmptyString(): (
+    value: string,
+    index: number,
+    array: string[]
+  ) => boolean {
+    return (word) => {
+      return word !== "";
+    };
   }
 }
 
