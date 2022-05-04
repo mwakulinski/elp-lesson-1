3401153e33c4bd29764f57acc4235b63d3d87722
mwakulinski <michal.wakulinski1@gmail.com>
2022-05-04 11:13:45 +0200

    implements squeeze() should join words and camelcase text by words

--- src/NokiaTexter.test.ts
+++ src/NokiaTexter.test.ts
@@ -1,7 +1,12 @@
 class NokiaTexter {
   constructor(private readonly text: string) {}
   squeeze(): string {
-    return this.text;
+    return this.text
+      .split(" ")
+      .map((word, index) => {
+        return index % 2 === 0 ? word.toUpperCase() : word.toLowerCase();
+      })
+      .join("");
   }
 }
 
@@ -12,5 +17,9 @@ describe("NokiaTexter Test", () => {
 
   it("should return the same text when text has no white spaces", () => {
     expect(new NokiaTexter("Jaksiemasz").squeeze()).toBe("Jaksiemasz");
+  });
+
+  it("should join words and camelcase text by words", () => {
+    expect(new NokiaTexter("Jak sie masz").squeeze()).toBe("JAKsieMASZ");
   });
 });
