b86a3bd7f03395eef1cc02c3c8235caea9725f3f
mwakulinski <michal.wakulinski1@gmail.com>
2022-05-04 11:03:10 +0200

    implements aqueeze() should return the same text when text has no white spaces

--- src/NokiaTexter.test.ts
+++ src/NokiaTexter.test.ts
@@ -1,9 +1,16 @@
 class NokiaTexter {
   constructor(private readonly text: string) {}
+  squeeze(): string {
+    return this.text;
+  }
 }
 
 describe("NokiaTexter Test", () => {
   it("should correctly define class instance", () => {
     expect(new NokiaTexter("Hi it should work")).toBeDefined();
+  });
+
+  it("should return the same text when text has no white spaces", () => {
+    expect(new NokiaTexter("Jaksiemasz").squeeze()).toBe("Jaksiemasz");
   });
 });
