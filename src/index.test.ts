ce997ebea82f2890d468fb35de51af8180773ee4
mwakulinski <michal.wakulinski1@gmail.com>
2022-05-04 09:29:04 +0200

    implements should correctly return result value

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,10 +3,18 @@ import { number } from "./index";
 describe("turboNumber tests", function () {
   class TurboNumber {
     constructor(private readonly number: number) {}
+    result(): number {
+      return this.number;
+    }
   }
 
   it("should initiate an class instance", () => {
     const turboNumber = new TurboNumber(1);
     expect(turboNumber).toBeTruthy;
+  });
+
+  it("should correctly return result value", () => {
+    const turboNumber = new TurboNumber(1);
+    expect(turboNumber.result()).toBe(1);
   });
 });
