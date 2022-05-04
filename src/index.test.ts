48b34922c73b82e2649f216126e7cce0a0c55c14
mwakulinski <michal.wakulinski1@gmail.com>
2022-05-04 09:44:17 +0200

    implement methodchaining in TruboNumber

--- src/index.test.ts
+++ src/index.test.ts
@@ -8,13 +8,16 @@ describe("turboNumber tests", function () {
     }
     add(numberToAdd: number) {
       this.number += numberToAdd;
+      return this;
     }
     subtract(numberToSubtract: number) {
       this.number -= numberToSubtract;
+      return this;
     }
     divide(divider: number) {
       if (divider === 0) throw new Error("You can not divide by 0");
       this.number /= divider;
+      return this;
     }
   }
 
@@ -49,5 +52,10 @@ describe("turboNumber tests", function () {
   it("should throw error when divided by 0", () => {
     const turboNumber = new TurboNumber(1);
     expect(() => turboNumber.divide(0)).toThrow("You can not divide by 0");
+  });
+
+  it("should allow method chaining", () => {
+    const turboNumber = new TurboNumber(17);
+    expect(turboNumber.subtract(5).divide(-3).result()).toBe(-4);
   });
 });
