55d6e016210e89500f30dcb41718aa156e8c1f9e
mwakulinski <michal.wakulinski1@gmail.com>
2022-05-04 09:32:24 +0200

    implement add method to TurboNumber

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,9 +2,12 @@ import { number } from "./index";
 
 describe("turboNumber tests", function () {
   class TurboNumber {
-    constructor(private readonly number: number) {}
+    constructor(private number: number) {}
     result(): number {
       return this.number;
+    }
+    add(numberToAdd: number) {
+      this.number += numberToAdd;
     }
   }
 
@@ -16,5 +19,11 @@ describe("turboNumber tests", function () {
   it("should correctly return result value", () => {
     const turboNumber = new TurboNumber(1);
     expect(turboNumber.result()).toBe(1);
+  });
+
+  it("should correctly sum numbers", () => {
+    const turboNumber = new TurboNumber(1);
+    turboNumber.add(4);
+    expect(turboNumber.result()).toBe(5);
   });
 });
